d3d0ac289fc29edf2433473bf67d59dce916437d
Mihika Sarwate <mihika@example.com>
2026-09-24 21:34:12 +0530

    fix: REAL assetPrefix

---
+++ out_temp/_next/static/yy3Tz9Pl4S2wsOOcHYrx4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Sudheer-Singh//_next/:path+",
+        "destination": "/Sudheer-Singh/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
